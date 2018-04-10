829a382f47440daf1b0c28fd3635bc5e3dbe2f3c
rafaelsm <rafaelsm89@gmail.com>
2018-04-09 23:30:24 -0300

    broken

--- app/src/androidTest/java/br/com/rads/myapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/br/com/rads/myapplication/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("br.com.rads.myapplication", appContext.packageName)
+        assertEquals("br.com.rads.meh", appContext.packageName)
     }
 }
