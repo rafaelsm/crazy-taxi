c8f58cbbd6d5c6f2b9ec3a4452a1227533775c88
rafaelsm <rafaelsm89@gmail.com>
2018-04-09 23:03:13 -0300

    project created

---
+++ android/app/src/main/java/br/com/rads/myapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.rads.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
